7d4185d06f5a0e4936e16ab51c915ac639c68e42
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-08 10:23:27 +0530

    learning for loop

---
+++ Loops/For Loop/Range.py
@@ -0,0 +1,12 @@
+for i in range(5):
+    print(i)
+
+
+#Question 1 count the numbe of i in word valirable
+word = "artificial intelligence"
+count =0
+for val in word:
+    if val == "i":
+        count+=1
+
+print(f"count is {count}")
---
+++ Loops/For Loop/basic.py
@@ -0,0 +1,9 @@
+string = "hello"
+
+#in memebership operator
+
+if 'o'in string:
+    print("'o' is exists")
+
+for var in string:
+    print(string)
