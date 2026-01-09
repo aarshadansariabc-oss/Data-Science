5075466b0c12e393b3f40656f4fdffe342dd817b
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-09 08:53:27 +0530

    strings

---
+++ Day 4/SlicingString.py
@@ -0,0 +1,7 @@
+wrod = "Currently i am learning about strings in python"
+#syntax str[st indx : end idx]
+
+print(wrod[0:len(wrod)])
+
+word2 = "Python"
+print(word2[-4:-2])
---
+++ Day 4/String.py
@@ -0,0 +1,7 @@
+word1 = "hello"
+word2 = "World"
+final = word1+word2
+print(final)
+
+for ch in final:
+    print(ch)
