25572f7c9b99a088d7887bdd15c2bd5adb2f9827
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-11 09:19:40 +0530

    Learnig about sets methods

---
+++ Data Types/Sets/methods.py
@@ -0,0 +1,18 @@
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 8, 9, 10}
+
+# s.add(6)
+# print(s)
+
+# s.remove(1)
+# print(s)
+
+# # s.clear() #remove all emenet in the set
+# # print(s)
+
+# s.pop() #it removes the random val
+# print(s)
+print("uninon")
+print(s1.union(s2))
+
+print(s1.intersection(s2))
