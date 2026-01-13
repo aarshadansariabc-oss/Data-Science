d52124563ccd582c7cd78c714bafda23a1df7ddc
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-13 12:57:32 +0530

    Solving problem 1 of class and object

--- Class & Object/p1.py
+++ Class & Object/p1.py
@@ -1,10 +1,10 @@
 class Product:
     count = 0
 
-    def __init__(self,name, price):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
-        Product.count +=1
+        Product.count += 1
 
     def get_info(self):
         print(f"Price of {self.name} is Rs. {self.price}")
@@ -14,17 +14,16 @@ class Product:
         print(f"Total object is {cls.count}")
 
     @staticmethod
-    def get_discount(price,discount):
+    def get_discount(price, discount):
         final_price = price - (discount*price/100)
         print(f"The final price is {final_price}")
 
 
-p1 = Product("Phone",10_000)
-p2 = Product("Laptop",50_000)
-p3 = Product("Register",100)
+p1 = Product("Phone", 10_000)
+p2 = Product("Laptop", 50_000)
+p3 = Product("Register", 100)
 
 p1.get_info()
 Product.get_count()
 
-p1.get_discount(p1.price,10)
-    
+p1.get_discount(p1.price, 12)
