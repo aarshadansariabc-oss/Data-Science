609340dc73ca3554fdee1d4cd175c7e309527e60
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-23 11:52:50 +0530

    doeing metods assignment

---
+++ Class & Object/opp Pillars/Encapsulation.py
@@ -0,0 +1,26 @@
+#Wrapping data & function into single unit
+
+# _balance => for Protected
+# __balance => for Private
+class Bank_Account:
+
+    def __init__(self,name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def get_Balance(self): #getter
+        return self.__balance
+    
+    def new_Balance(self,Add_Balance):
+        self.__balance = Add_Balance
+
+
+acc1 = Bank_Account("Shakid",15000)
+
+# print(acc1.name, acc1.balance)
+print(acc1.name, acc1.get_Balance())
+print(acc1.name, acc1._Bank_Account__balance)
+
+
+        
+    
