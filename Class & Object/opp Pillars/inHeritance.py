e20677c053f54f24ed674f51d644ab083ae826f1
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-23 12:00:35 +0530

    doeing metods assignment

---
+++ Class & Object/opp Pillars/inHeritance.py
@@ -0,0 +1,13 @@
+class Parent:
+        def P(self):
+                    print("I am Your Parent")
+
+class Child(Parent):
+        def c(self):
+            print("THis is chiled class")
+
+s1 = Child()
+
+s1.P()
+
+        
