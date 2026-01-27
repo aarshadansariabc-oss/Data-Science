5bfbf5312e7ef4f94b8b0b9246f6fc03ce12f2ee
Arshad Ansari <aarshadansariabc@gmail.com>
2026-01-27 12:54:20 +0530

    continue learning about data science

--- Class & Object/opp Pillars/Abstraction.py
+++ Class & Object/opp Pillars/Abstraction.py
@@ -1,1 +1,22 @@
-#Abstraction -: Hiding internal details & showing only essential features
+#Abstraction :- Hiding internal details & showing only essential features
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound():
+        pass
+
+class Lion(Animal):
+    def make_sound(self):
+        print("Roar")
+
+class Dog(Animal):
+    def make_sound(self):
+        print("bho bho")
+
+
+dog = Dog()
+lion = Lion()
+
+lion.make_sound()
+dog.make_sound()
